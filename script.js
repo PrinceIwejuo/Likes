e61f2df5e24584e84aa30b31646f4198c9e0ba11
PrinceIwejuo <Princeiwejuo@gmail.com>
2022-02-02 00:47:41 -0500

    LikesProject

---
+++ script.js
@@ -0,0 +1,20 @@
+var likesCount = document.querySelector('#likesCount')
+console.log(likesCount);
+
+function increaseCount(){
+    likesCount.innerText++;
+}
+
+var likesCount2 = document.querySelector('#likesCount2')
+console.log(likesCount2);
+
+function increaseCount2(){
+    likesCount2.innerText++;
+}
+
+var likesCount3 = document.querySelector('#likesCount3')
+console.log(likesCount3);
+
+function increaseCount3(){
+    likesCount3.innerText++;
+}
